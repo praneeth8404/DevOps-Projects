fa0859d9d2d1557682efe4176d86df2c56b5d40e
praneeth8404 <62732347+praneeth8404@users.noreply.github.com>
2024-11-21 21:31:42 +0530

    Update ec2-create.sh

--- AWS/aws-cli/ec2-create.sh
+++ AWS/aws-cli/ec2-create.sh
@@ -1,8 +1,25 @@
+#!/bin/bash
+
+# Variables
+AMI_ID=$1     
+INSTANCE_TYPE=$2               
+KEY_NAME=$3                 
+SECURITY_GROUP_ID=$4
+SUBNET_ID=$5   
+INSTANCE_NAME=$6      
+
+# Create EC2 instance
 aws ec2 run-instances \
-    --image-id <AMI_ID> \
+    --image-id "$AMI_ID" \
     --count 1 \
-    --instance-type <INSTANCE_TYPE> \
-    --key-name <KEY_PAIR_NAME> \
-    --security-group-ids <SECURITY_GROUP_ID> \
-    --subnet-id <SUBNET_ID> \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=<INSTANCE_NAME>}]'
+    --instance-type "$INSTANCE_TYPE" \
+    --key-name "$KEY_NAME" \
+    --security-group-ids "$SECURITY_GROUP_ID" \
+    --subnet-id "$SUBNET_ID" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" \
+    --output json
+
+# Print confirmation message
+echo "EC2 instance creation initiated."
+
+
